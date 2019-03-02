dd2696bbcbbbf25e8131da60f43a796f9c2aeea0
jinlow <james.d.inlow@gmail.com>
2019-03-02 08:24:31 -0600

    Adding read birth data functionality

--- web_prep/nber_retrieve_vts_births.R
+++ web_prep/nber_retrieve_vts_births.R
@@ -70,17 +70,101 @@ sec1_dls <- get_links(link = vts_sub_links[[1]], file_extension = ".zip")
 sec1_dls <- sec1_dls[stringr::str_sub(sec1_dls, start = -8) == ".csv.zip"]
 
 # Function for reading in zipped CSVs
-# WARNING: As is this function downloads file into the working directory 
-# and deletes it... Probably need to create a better solution. Or have it 
-# download into a temporary directory somewhere.
-read_ziplink <- function(link) {
+# The function creates a temporary file and directory, 
+# then it deletes the temporary file once it has been 
+# loaded into memory. The tempory directory is deleted 
+# if R closes in a standard fashion.
+read_ziplink <- function(link, format = "csv") {
   temp <- tempfile()
-  download.file(link, temp)
-  temp <- unzip(temp)
-  data <- fread(temp)
+  download.file(link, temp, mode = "wb")
+  temp <- unzip(temp, exdir = tempdir())
+  if (format == "csv") {
+    dt <- fread(temp)
+  } else if (format == "sas") {
+    dt <- as.data.table(haven::read_sas(temp))
+  } else if (format == "stata") {
+    dt <- as.data.table(haven::read_dta(temp))
+  }
   unlink(temp)
-  return(data)
+  return(dt)
 }
 
-# Can be slow for large datasets. 
-test <- read_ziplink(sec1_dls[[1]])
+# Funtion works for SAS, Stata, and csv data types as demonstrated below.  
+test <- read_ziplink(link = sec1_dls[[1]])
+test2 <- read_ziplink(link = "https://www.nber.org/lbid/2013/linkco2013us_num.sas7bdat.zip", 
+                      format = "sas")
+test3 <- read_ziplink(link = "https://www.nber.org/lbid/2013/linkco2013us_num.dta.zip", 
+                      format = "stata")
+
+## The types of data in section 1
+# 1.1 Linked Deaths: format linkcoYYYYus_num.csv.zip
+# 1.2 Births: format linkcoYYYYus_den.csv.zip
+# 1.3 Unliked Deaths: format linkcoYYYYus_unl.csv.zip
+# 1.4 US Territories Linked Deaths: format linkcoYYYYps_num.csv.zip
+# 1.5 US Territories Births: format linkcoYYYYps_den.csv.zip
+# 1.6 US Territories Unliked Deaths: format linkcoYYYYps_unl.csv.zip
+
+### WARNINGs: Many of the 1.6 section data pages are missing, or broken,
+#   need to figure out how to just have it throw a generic warning like, 
+#   "Data for the given year doesn't exist"
+#   Also: Some of this data is very big, resulting in a lot of large data
+#         so we will have to be cautious as we read in large data sets.
+#         It seems section 1.2 is rather large.
+
+## Read Birth Cohort Linked Birth/Infant Death Data Function
+
+read_vtsb_bcl <- function(years, 
+                          section,
+                          links = sec1_dls) {
+  # Name list 
+  names(years) <- paste0("d_", years, "_", 
+                         gsub("[:space:]", 
+                              x = section, 
+                              replacement = "_"))
+  # subset years
+  links <- lapply(years, function(year) {grep(pattern = year, 
+                              x = links, 
+                              value = TRUE)})
+  links <- unlist(links)
+  
+  # subset data.section
+  if (section %in% c("linked deaths", "1.1")) {
+    links <- grep(pattern = "us_num", x = links, value = TRUE)
+    
+  } else if (section %in% c("births", "1.2")) {
+    links <- grep(pattern = "us_den", x = links, value = TRUE)
+    
+  } else if (section %in% c("unliked deaths", "1.3")) {
+    links <- grep(pattern = "us_unl", x = links, value = TRUE)
+    
+  } else if (section %in% c("linked deaths us terr", "1.4")) {
+    links <- grep(pattern = "ps_unl", x = links, value = TRUE)
+    
+  } else if (section %in% c("births us terr", "1.5")) {
+    links <- grep(pattern = "ps_den", x = links, value = TRUE)
+    
+  } else if (section %in% c("unliked deaths us terr", "1.6")) {
+    links <- grep(pattern = "ps_unl", x = links, value = TRUE)
+  }
+  
+  # read list of tables
+  links <- unlist(links)
+  dt <- lapply(links, read_ziplink)
+  return(dt)
+}
+
+# This way the user can request select years
+sec1_1_dat <- read_vtsb_bcl(years = c(1995, 2013), 
+                          section = "1.1")
+
+# Or a vector of sequential years
+# This also shows how if a data year does not exist, the 
+# function will not return a warning, but rather will just
+# return the data for the years that are present.
+sec1_2_dat <- read_vtsb_bcl(years = 1990:1995, 
+                          section = "1.1")
+
+# Because we named the list we can call tables by name
+sec1_1_dat$d_1995_1.11[1:10, 1:5]
+
+
