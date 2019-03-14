8e15596124c33cef62939cb3bbce3f17beb1f79b
Hussung <599249@bah.com>
2019-03-14 17:23:30 -0400

    cleaning up work to set up a fars.link.table

--- data-raw/fars_link_raw.R
+++ data-raw/fars_link_raw.R
@@ -40,7 +40,7 @@ fars_table <- as.data.table(fars_links) %>%
   .[ , 
      c('accident.url', 'vehicle.url', 'person.url') := lapply(X = .[ , c('accident.url', 'vehicle.url', 'person.url'), with = F],
                                                               FUN = function(x) paste0('https://www.nber.org', x))]
-# Get accompanying documentation -- level 1
+# Get accompanying documentation 
 get_fars_desc <- function(pattern){
   
   read_html(fars.url) %>%
@@ -50,7 +50,7 @@ get_fars_desc <- function(pattern){
   
 }
 
-fars_desc <- fars_links <- lapply(c('accident', 'vehicle', 'person'), 
+fars_desc <- lapply(c('accident', 'vehicle', 'person'), 
                                   FUN = function(x) get_fars_desc(x)) 
 
 names(fars_desc) <- c('accident.desc', 'vehicle.desc', 'person.desc')
@@ -62,3 +62,20 @@ fars_desc <- as.data.table(fars_desc) %>%
   .[ , 
      c('accident.desc', 'vehicle.desc', 'person.desc') := lapply(X = .[ , c('accident.desc', 'vehicle.desc', 'person.desc'), with = F],
                                                               FUN = function(x) paste0('https://www.nber.org', x, '/desc.txt'))]
+
+# Now we have a table of links to data and links to accompanying documentation
+ # -- combine them!
+
+fars_melted_table <- fars_table %>%
+  melt(., id.vars = c('Year'), measure.vars = setdiff(names(.), 'Year'),
+       variable.name = 'dataset', value.name = 'url') %>%
+  .[ , dataset := substr(x = as.character(dataset), start = 1, stop = (nchar(as.character(dataset)) - 4))]
+
+fars_melted_desc <- fars_desc %>%
+  melt(., id.vars = c('Year'), measure.vars = setdiff(names(.), 'Year'),
+       variable.name = 'dataset', value.name = 'description.url') %>%
+  .[ , dataset := substr(x = as.character(dataset), start = 1, stop = (nchar(as.character(dataset)) - 5))]
+
+fars.link.table <- merge(fars_melted_table,
+                         fars_melted_desc,
+                         by = c('Year', 'dataset'))
